02a1720c097378760edeced4d8bc11da66af3422
Malaika Shabbir <malaikashabbir1@gmail.com>
2026-09-09 21:35:08 +0500

    Add GET endpoints for books

--- Exercise_2_CRUD_using_Express/app.js
+++ Exercise_2_CRUD_using_Express/app.js
@@ -1,18 +1,77 @@
 const express = require("express");
 const app = express();
 
-app.use( (req,res,next) => {
+// ___________ For parsing JSON body
+app.use(express.json())
+
+const books = [
+    {
+        id: 1,
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        price: 500
+    },
+    {
+        id: 2,
+        title: "Atomic Habits",
+        author: "James Clear",
+        price: 800
+    },
+    {
+        id: 3,
+        title: "1984",
+        author: "George Orwell",
+        price: 600
+    },
+    {
+        id: 4,
+        title: "The Psychology of Money",
+        author: "Morgan Housel",
+        price: 700
+    },
+    {
+        id: 5,
+        title: "Rich Dad Poor Dad",
+        author: "Robert Kiyosaki",
+        price: 550
+    }
+];
+
+
+app.get( "/", (req,res,next) => {
     console.log("FIRST MIDDLEWARE", req.url, req.method);
-    next();
+    res.send("FIRST MIDDLEWARE");
 })
 
-app.use( (req,res,next) => {
+app.use( "/submit" , (req,res,next) => {
     console.log("SECOND MIDDLEWARE", req.url, req.method);
-    next();
+    res.send("SECOND MIDDLEWARE");
 })
 
-app.get("/home", (req,res) => {
-    res.json({message: "WELCOME TO HOME"})
+// ____________________ GET ALL BOOKS ___________________
+app.get("/books", (req,res) => {
+    res.status(200).json(books)
+})
+
+// ________________ GET A BOOK BY ID by uisng req.params __________________
+app.get("/books/:id" , (req,res) => {
+    
+    // req.params.id → gets the id from the URL
+    const ID = Number(req.params.id);
+
+    if(isNaN(ID)) {
+        return res.status(400).json({message: "Invalid ID"})
+    }
+
+    const book = books.find( (book) => {
+        return book.id === ID 
+    })
+
+    if(!book) {
+        return res.status(404).json({message: "Book not Found"})
+    }
+
+    res.status(200).json(book)  
 })
 
 const PORT = 3000;
