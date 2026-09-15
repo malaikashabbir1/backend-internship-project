4a0347972a64a618b1fcec59d5382f8443c72de1
Malaika Shabbir <malaikashabbir1@gmail.com>
2026-09-15 21:25:23 +0500

    Create user model and schema

---
+++ Exercise5_userAuthentication_and_Authorization/models/userModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema( {
+    name: String,
+    email: String,
+    password: String,
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
+    }
+})
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
